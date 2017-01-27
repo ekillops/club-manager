19592af2158b38d8ddbd04b4a20ffc2795f9564f
Erik Killops <erik.killops@gmail.com>
2017-01-27 08:59:54 -0800

    add routing and firebase config

---
+++ src/app/api-keys-example.ts
@@ -0,0 +1,15 @@
+export const masterFirebaseConfig = {
+  apiKey: "xxxx",
+  authDomain: "xxxx.firebaseapp.com",
+  databaseURL: "https://xxxx.firebaseio.com",
+  storageBucket: "xxxx.appspot.com",
+  messagingSenderId: "xxxx"
+}
+
+/*
+1. Replace the values above with values from your own firebase project
+
+2. Rename this file from 'api-keys-example.ts' to 'api-keys.ts'
+
+3. Optionally, import 'data.json' to your firebase project to seed it with data
+*/
