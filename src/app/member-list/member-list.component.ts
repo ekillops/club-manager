0eb5b0b0283aa7f6eabf48d734790d47ad70ce49
Erik Killops <erik.killops@gmail.com>
2017-01-27 11:38:58 -0800

    add content for member list component

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Router } from '@angular/router';
+import { Member } from '../models/member.model';
+import { MemberService } from '../services/member.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
+  providers: [MemberService]
 })
 export class MemberListComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
+  }
+
+  viewMemberPage(memberIdKey:string): void {
+    this.router.navigate(['members', memberIdKey]);
   }
 
 }
